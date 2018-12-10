c488703c86d50f5546da117e711e88ca0dd9340b
Jguer <me@jguer.space>
2018-12-10 01:06:50 +0000

    First version

--- asterisk/stock.py
+++ asterisk/stock.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+
+import sys
+import re
+import time
+import random
 import json
 import urllib.request
 
@@ -9,14 +15,22 @@ MESSAGE = {
     }]
 }
 
+def saynumber (params):
+   sys.stderr.write("SAY NUMBER %s \"\"\n" % params)
+   sys.stderr.flush()
+   sys.stdout.write("SAY NUMBER %s \"\"\n" % params)
+   sys.stdout.flush()
+   result = sys.stdin.readline().strip()
+   checkresult(result)
+
 STOCK_URL = "http://gateway.euronext.com/api/ticker/instrumentDetail"
 req = urllib.request.Request(STOCK_URL)
 req.add_header('Content-Type', 'application/json; charset=utf-8')
 jsondata = json.dumps(MESSAGE)
 jsondataasbytes = jsondata.encode('utf-8')  # needs to be bytes
 req.add_header('Content-Length', len(jsondataasbytes))
-print(jsondataasbytes)
 r = urllib.request.urlopen(req, jsondataasbytes)
 data = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
 # print(data)
-print(data["results"][0]["lastPrice"])
+# print(data["results"][0]["lastPrice"])
+saynumber(data["results"][0]["lastPrice"])
